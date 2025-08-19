e932da973b1029b8c0a54436b754c29957d68d4b
Pasha Leshkevich <sum.infinite.jest@gmail.com>
2025-08-19 13:50:30 +0300

    1.5. Поля объекта [#166083 #477859] - practice

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,29 @@
 package ru.job4j.oop;
 
 public class Cat {
-    public static void main(String[] args) {
-        Cat petty = new Cat();
-        Cat sparky = new Cat();
+    private String name;
+    private String food;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(name + " eats " + food + ". ");
+    }
+
+        public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("catlet");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
